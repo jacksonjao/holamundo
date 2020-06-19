fd50d376316f41fa9d1b5607ed7e508d5563ed5e
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-06-19 00:21:53 -0500

    Se corrigió el archivo text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:holamundo/main.dart';
+import 'package:holamundo/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
